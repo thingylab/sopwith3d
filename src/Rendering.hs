cc43005a3f301768cd2f9e522fd2e31dc4257f45
Pierre DOUCY <pierre.doucy@gmail.com>
2009-02-19 00:29:16 -0500

    Just a test - sorry.

--- src/Rendering.hs
+++ src/Rendering.hs
@@ -1,3 +1,28 @@
+{-
+Copyright (c) 2009 Pierre DOUCY
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+-}
+
 module Rendering(
 					renderObj,
 					ObjLine(ObjV, ObjVn, ObjF, OtherStuff)) where
@@ -18,7 +43,9 @@ data ObjLine = ObjV (Vertex3 GLfloat)
 {-
 	All these lookups in renderFace are very inefficient.
 	TODO: transform the ObjLine object first in a friendlier list
-	that can easily be mapM_-ed.
+	that can easily be mapM_-ed. Also, use only triangulated surfaces.
+	
+	Note: faces should be set to smooth in blender to get the proper normals.
 -}
 renderObj (vs,ns, fs) =
 	mapM_ (renderFace vs ns) fs
@@ -26,8 +53,14 @@ renderObj (vs,ns, fs) =
 renderFace vs ns f@(ObjF vi ni) =
 	case length vi of
 		3 -> renderPrimitive Triangles $ do
-			mapM_ vertex vertices
-			mapM_ normal normals
+			--mapM_ vertex vertices
+			--mapM_ normal normals
+			normal $ normals !! 0
+			vertex $ vertices !! 0
+			normal $ normals !! 1
+			vertex $ vertices !! 1
+			normal $ normals !! 2
+			vertex $ vertices !! 2
 		4 -> renderPrimitive Quads $ do
 			mapM_ vertex vertices
 			mapM_ normal normals
@@ -44,11 +77,3 @@ findNormals ns nn =
 	n
 	where
 		ObjVn n = ns !! (nn - 1)
-
-{-renderFileObject (FileObject fo) =
-	mapM_ renderFace fo
-
-renderFace (FaceTriangle vs) =
-	renderPrimitive Triangles $ mapM_ vertex vs
-renderFace (FaceQuad vs) =
-	renderPrimitive Quads $ mapM_ vertex vs-}
--- src/Sopwith.hs
+++ src/Sopwith.hs
@@ -1,3 +1,28 @@
+{-
+Copyright (c) 2009 Pierre DOUCY
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+-}
+
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
 import Data.IORef
@@ -27,17 +52,18 @@ main = do
 -- TODO: Make sense of it and move somewhere else
 myInit :: IO ()
 myInit = do
-   clearColor $= Color4 0 0 0 0
-   shadeModel $= Smooth
+	clearColor $= Color4 0 0 0 0
+	normalize $= Enabled
 
-   position (Light 0) $= Vertex4 20 (20) (10.0::GLfloat) 0
+	position (Light 0) $= Vertex4 20 (20) (10.0::GLfloat) 0
 
-   ambient (Light 1) $= Color4 1 1 1 1
+	ambient (Light 1) $= Color4 1 1 1 1
 
-   lighting $= Enabled
-   light (Light 0) $= Enabled
-   light (Light 1) $= Enabled
-   depthFunc $= Just Less
+	lighting $= Enabled
+	light (Light 0) $= Enabled
+	light (Light 1) $= Enabled
+	shadeModel $= Smooth
+	depthFunc $= Just Less
 
 -- TODO: move
 reshape :: ReshapeCallback
